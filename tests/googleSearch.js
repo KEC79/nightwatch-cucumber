1c51b6c67467d442e9baed473bc94328579c5efd
Kim <mrsshoes2@gmail.com>
2020-02-08 12:52:16 +0000

    Add POM support

---
+++ tests/googleSearch.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "Google search test": function(browser) {
+    var google = browser.page.google();
+
+    google.navigate()
+      .waitForElementVisible("body")
+      .assert.titleContains("Google")
+      .assert.visible("@searchBar")
+      .setValue("@searchBar", "nightwatch")
+      .assert.visible("@submit")
+      .click("@submit")
+      .assert.titleContains("nightwatch - Google Search")
+      .end();
+  }
+};
